e14194b9a6493b7957e6177536da049170f6f368
Jiachen <zhangjiachen6@gmail.com>
2020-04-29 21:28:25 -0700

    Updates

---
+++ precache-manifest.73b7286c1ae17aa75ec3b380bf7d50ad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c052ad948e86884491f388e4c2e6b32",
+    "url": "/cs185/index.html"
+  },
+  {
+    "revision": "6c5bd80f42038b8113ba",
+    "url": "/cs185/static/css/main.eb5c07b8.chunk.css"
+  },
+  {
+    "revision": "f711eebed1f91880a17a",
+    "url": "/cs185/static/js/2.4dbc6191.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cs185/static/js/2.4dbc6191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5bd80f42038b8113ba",
+    "url": "/cs185/static/js/main.b031307b.chunk.js"
+  },
+  {
+    "revision": "fc78b63c9fb27686a23e",
+    "url": "/cs185/static/js/runtime-main.cdb40490.js"
+  },
+  {
+    "revision": "0f98dca45d02168df658e84272ae7e73",
+    "url": "/cs185/static/media/image1.0f98dca4.jpg"
+  },
+  {
+    "revision": "6c5451a5dc665ee546353995dc1970a9",
+    "url": "/cs185/static/media/image2.6c5451a5.png"
+  },
+  {
+    "revision": "d4243c355df889538c9beec639b039ed",
+    "url": "/cs185/static/media/image3.d4243c35.jpg"
+  },
+  {
+    "revision": "2ec6886045b91bff05a977548c739dfe",
+    "url": "/cs185/static/media/image4.2ec68860.jpg"
+  },
+  {
+    "revision": "9a637c0fcfd45a595faa2499bd453d7e",
+    "url": "/cs185/static/media/image5.9a637c0f.jpg"
+  },
+  {
+    "revision": "cd3ff6e29f2f3ffea920e1f3a2dda794",
+    "url": "/cs185/static/media/image6.cd3ff6e2.jpg"
+  },
+  {
+    "revision": "8bdf300910d71eee6497c3d94bda5e9f",
+    "url": "/cs185/static/media/image7.8bdf3009.jpg"
+  },
+  {
+    "revision": "f3a60cf419a8283ce484ad2740755059",
+    "url": "/cs185/static/media/image8.f3a60cf4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cs185/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cs185/precache-manifest.73b7286c1ae17aa75ec3b380bf7d50ad.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cs185/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
