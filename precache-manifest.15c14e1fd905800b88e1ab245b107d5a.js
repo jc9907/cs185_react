c1547ac30b893ad0a211dd8c7781cc43d14ac379
Jiachen <zhangjiachen6@gmail.com>
2020-05-17 20:18:23 -0700

    Updates

---
+++ precache-manifest.15c14e1fd905800b88e1ab245b107d5a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca6dde3bdef51dee6d57a2f9c5d308b",
+    "url": "/cs185_react/index.html"
+  },
+  {
+    "revision": "379edd05115b74126d6c",
+    "url": "/cs185_react/static/css/main.a0987033.chunk.css"
+  },
+  {
+    "revision": "0dfef6db16008632500b",
+    "url": "/cs185_react/static/js/2.9e0526ef.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/cs185_react/static/js/2.9e0526ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379edd05115b74126d6c",
+    "url": "/cs185_react/static/js/main.1b43242b.chunk.js"
+  },
+  {
+    "revision": "be51dbb9a70355aa78f3",
+    "url": "/cs185_react/static/js/runtime-main.3b751172.js"
+  },
+  {
+    "revision": "0f98dca45d02168df658e84272ae7e73",
+    "url": "/cs185_react/static/media/image1.0f98dca4.jpg"
+  },
+  {
+    "revision": "6c5451a5dc665ee546353995dc1970a9",
+    "url": "/cs185_react/static/media/image2.6c5451a5.png"
+  },
+  {
+    "revision": "d4243c355df889538c9beec639b039ed",
+    "url": "/cs185_react/static/media/image3.d4243c35.jpg"
+  },
+  {
+    "revision": "2ec6886045b91bff05a977548c739dfe",
+    "url": "/cs185_react/static/media/image4.2ec68860.jpg"
+  },
+  {
+    "revision": "9a637c0fcfd45a595faa2499bd453d7e",
+    "url": "/cs185_react/static/media/image5.9a637c0f.jpg"
+  },
+  {
+    "revision": "cd3ff6e29f2f3ffea920e1f3a2dda794",
+    "url": "/cs185_react/static/media/image6.cd3ff6e2.jpg"
+  },
+  {
+    "revision": "8bdf300910d71eee6497c3d94bda5e9f",
+    "url": "/cs185_react/static/media/image7.8bdf3009.jpg"
+  },
+  {
+    "revision": "f3a60cf419a8283ce484ad2740755059",
+    "url": "/cs185_react/static/media/image8.f3a60cf4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cs185_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185_react/precache-manifest.848d08cb96d44a0ebaef14b82a1e41b7.js"
+  "/cs185_react/precache-manifest.15c14e1fd905800b88e1ab245b107d5a.js"
 );
 
 self.addEventListener('message', (event) => {
